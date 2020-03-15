c3e47c8ead133e7d7a1759ab6ff31dec70d1ca0a
Welton Santos <weltonsantos@dcc.ufmg.br>
2020-03-15 09:54:38 -0300

    1. Adição de rotas estáticas no arquivo settings.py (servir arquivos estáticos). 2. Adição de arquivos css, js para customização de timeline. 3. Teste realizado com Timeline.js usando JSON funcionando muito bem aparentemente.

--- interface/storytracker/views.py
+++ interface/storytracker/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.db import models
 
 # Create your views here.
+
+class TimeLine():
+
+    def home(request):
+
+        return render(request, 'timeline.html')
